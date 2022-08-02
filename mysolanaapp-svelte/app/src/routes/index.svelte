31d029095186b417b0ffcc4b8f810d9e32a371ce
KyleTruong <ktruong008@gmail.com>
2022-08-02 15:39:24 -0400

    sign message

--- mysolanaapp-svelte/app/src/routes/index.svelte
+++ mysolanaapp-svelte/app/src/routes/index.svelte
@@ -1,4 +1,18 @@
 <script lang="ts">
+	import { walletStore as walletStore$ } from '@svelte-on-solana/wallet-adapter-core';
+	import { onMount } from 'svelte';
+
+	$: {
+		console.log('$walletStore$: ', $walletStore$);
+	}
+
+	onMount(async () => {
+		const dv = new Uint8Array([1, 2, 3]);
+
+		const what = await $walletStore$?.signMessage?.(dv);
+
+		console.log('what: ', what);
+	});
 </script>
 
 <div>
